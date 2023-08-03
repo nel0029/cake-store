cdd49d50d9b88b352f95ffa3a4497a1cf8b66689
jaynel29 <jaynellucanas@gmail.com>
2023-08-03 21:44:51 +0800

    Finished 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /** @format */
 
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react()],
 });
