2aa2d4d414ded283e6f23cf792ce2998e70cf9ca
jaynel29 <jaynellucanas@gmail.com>
2023-09-15 22:57:25 +0800

    Gallery pictures has been reduced

--- src/data/Products.ts
+++ src/data/Products.ts
@@ -10,10 +10,4 @@ export const dummyProducts = [
   { image: "./gallery/07.webp" },
   { image: "./gallery/08.webp" },
   { image: "./gallery/09.webp" },
-  { image: "./gallery/10.webp" },
-  { image: "./gallery/11.webp" },
-  { image: "./gallery/12.webp" },
-  { image: "./gallery/13.webp" },
-  { image: "./gallery/14.webp" },
-  { image: "./gallery/15.webp" },
 ];
